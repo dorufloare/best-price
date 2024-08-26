52a39ee676897394fb09636133b8903804c8c9fa
dorufloare <dorufloare@yahoo.com>
2024-08-26 18:47:37 +0300

    clerk webhook

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -37,3 +37,44 @@ export async function getUserById(userId: string) {
     console.log(error);
   }
 }
+
+export async function updateUser(clerkId: string, user: {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new Error("User update failed");
+    
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase();
+
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error("User not found");
+    }
+
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    console.log(error);
+  }
+}
