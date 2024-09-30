f517af817d680a0508bd07e3733fb06a7612c4e1
Doru Floare <dorufloare@yahoo.com>
2024-09-30 18:19:09 +0300

    placeholder image 2.0

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -9,17 +9,13 @@ import { useEffect, useState } from 'react';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+  DialogTitle
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from '@/components/ui/button';
 import { getUserById, removeProductFromUser } from '@/lib/actions/user.actions';
 import { removeProductFromCache } from '@/lib/cache.utils';
+
 const ProductPage: React.FC = () => {
   const { userId } = useAuth();
   const params = useParams();
@@ -28,63 +24,68 @@ const ProductPage: React.FC = () => {
   const [product, setProduct] = useState<ProductData>();
   const [open, setOpen] = useState(false);
   const [user, setUser] = useState<UserData | null>(null);
+  
+  // State for handling image URL and fallback
+  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined);
 
   const fetchProductData = async () => {
     try {
       if (productId) {
         const fetchedProduct = await getProductById(productId);
         setProduct(fetchedProduct);
+        setImgSrc(fetchedProduct.imageUrl); // Set initial image URL
       }
     } catch (error) {
       console.log('Error fetching product data');
     }
   };
 
-  const fetchUser = async() => {
+  const fetchUser = async () => {
     try {
-      if (!userId)
-        return;
+      if (!userId) return;
       const fetchedUser = await getUserById(userId);
       setUser(fetchedUser);
-
-    } catch(error) {
-      console.log('Error fetching user')
+    } catch (error) {
+      console.log('Error fetching user');
     }
-  }
+  };
 
   const handleDelete = () => {
-    if (!product?._id || !userId)
-      return;
-
+    if (!product?._id || !userId) return;
+    
     removeProductFromUser(userId, product._id);
-    removeProductFromCache(product?._id);
+    removeProductFromCache(product._id);
 
     setOpen(false);
     router.push('/dashboard');
+  };
+
+  // Handle image error and set fallback
+  const handleImageError = () => {
+    setImgSrc('/assets/images/placeholder.jpg');
   };
 
   useEffect(() => {
     if (!userId) redirect('/sign-in');
     fetchUser();
     fetchProductData();
-  }, []);
+  }, [userId, productId]);
 
-  if (!product || !productId) 
-    return <p>Loading...</p>;
-  if (!user || !user.products || !user.products.includes(productId)) 
-    return <p>Unauthorized</p>;
-  
+  if (!product || !productId) return <p>Loading...</p>;
+  if (!user || !user.products || !user.products.includes(productId)) return <p>Unauthorized</p>;
+
   return (
     <div className="mx-[5%] md:mx-[10%] lg:mx-[16%] mt-2 md:mt-8 lg:mt-16 h-screen">
       <div className="flex flex-col lg:flex-row gap-2 lg:gap-16 w-full">
         <div className="flex justify-center h-[20rem] md:h-[100rem] lg:h-auto lg:w-[40%]">
           <div className="relative w-full h-[15rem] md:h-[30rem] lg:h-auto overflow-hidden mt-4 lg:mt-32 items-start"> 
             <Image
-              src={product.imageUrl}
+              src={imgSrc || '/assets/images/placeholder.jpg'}  // Use imgSrc for dynamic handling
               alt="product image"
-              layout="fill" 
-              objectFit="contain" 
-              objectPosition="top" 
+              layout="fill"
+              objectFit="contain"
+              objectPosition="top"
+              onError={handleImageError}  // Handle image load failure
             />
           </div>
         </div>
@@ -94,12 +95,10 @@ const ProductPage: React.FC = () => {
               {product.name}
             </h3>
             <p className='text-xs md:text-sm lg:text-sm text-blue-800 underline mt-3 md:tracking-wide mb-5'>
-              <a href={product.url}>
-                Visit product
-              </a>
+              <a href={product.url}>Visit product</a>
             </p>
           </div>
-          <PriceChart product={product}/>
+          <PriceChart product={product} />
         </div>
       </div>
       <Dialog open={open} onOpenChange={setOpen}>
@@ -112,7 +111,7 @@ const ProductPage: React.FC = () => {
         </DialogContent>
       </Dialog>
       <p className='text-xs md:text-sm lg:text-base text-red-500 mt-10'>
-        Not interested anymore? <span className='ml-2 underline cursor-pointer'  onClick={() => setOpen(true)}> Remove this product from your dashboard </span>
+        Not interested anymore? <span className='ml-2 underline cursor-pointer' onClick={() => setOpen(true)}>Remove this product from your dashboard</span>
       </p>
     </div>
   );
